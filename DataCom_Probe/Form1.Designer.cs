8d7966bbf8f13f63e399736e5f084c990eb37e45
netravler <pheintz@hotmail.com>
2018-09-13 11:25:08 -0500

    Add files via upload

---
+++ DataCom_Probe/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace DataCom_Probe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.probeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.probeToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.rAWToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cSVToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(226, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(461, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "https://connect.data.com/directory/company/list/jKpOTyCSHEU8JBeIGLJECA/people-s-u" +
+                "nited-bank";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(813, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Page(s):";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(22, 57);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(964, 297);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.probeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1009, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // probeToolStripMenuItem
+            // 
+            this.probeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.probeToolStripMenuItem1,
+            this.saveToolStripMenuItem1});
+            this.probeToolStripMenuItem.Name = "probeToolStripMenuItem";
+            this.probeToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.probeToolStripMenuItem.Text = "Probe";
+            this.probeToolStripMenuItem.Click += new System.EventHandler(this.probeToolStripMenuItem_Click);
+            // 
+            // probeToolStripMenuItem1
+            // 
+            this.probeToolStripMenuItem1.Name = "probeToolStripMenuItem1";
+            this.probeToolStripMenuItem1.Size = new System.Drawing.Size(117, 24);
+            this.probeToolStripMenuItem1.Text = "Probe";
+            this.probeToolStripMenuItem1.Click += new System.EventHandler(this.probeToolStripMenuItem1_Click);
+            // 
+            // saveToolStripMenuItem1
+            // 
+            this.saveToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rAWToolStripMenuItem1,
+            this.cSVToolStripMenuItem1});
+            this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(117, 24);
+            this.saveToolStripMenuItem1.Text = "Save";
+            // 
+            // rAWToolStripMenuItem1
+            // 
+            this.rAWToolStripMenuItem1.Name = "rAWToolStripMenuItem1";
+            this.rAWToolStripMenuItem1.Size = new System.Drawing.Size(111, 24);
+            this.rAWToolStripMenuItem1.Text = "RAW";
+            // 
+            // cSVToolStripMenuItem1
+            // 
+            this.cSVToolStripMenuItem1.Name = "cSVToolStripMenuItem1";
+            this.cSVToolStripMenuItem1.Size = new System.Drawing.Size(111, 24);
+            this.cSVToolStripMenuItem1.Text = "CSV";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(881, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "1";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(982, 0);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(27, 22);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 380);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "DataCom_Probe";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem probeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem probeToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem rAWToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem cSVToolStripMenuItem1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ DataCom_Probe/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//
+using System.Net;
+
+
+
+namespace DataCom_Probe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void probeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void probeToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            // ok this is a simple test app prior to creating a web service.
+            // first we get the webpage to probe then loop through incrementing page by page until 
+            // we get an error.  Ok yes the first pages does tell you the number of pages
+            // but I'm lazy deal with it.
+            /*
+             * this is a representitive sample and our test victim.
+             * page url decode:
+             *      first page https://connect.data.com/directory/company/list/jKpOTyCSHEU8JBeIGLJECA/people-s-united-bank
+             *      each following page notice the page count on the end: https://connect.data.com/directory/company/list/jKpOTyCSHEU8JBeIGLJECA/people-s-united-bank?page=2
+            */
+
+            int pageNumber = 1;
+            string probePage = textBox1.Text;
+
+            try
+            {
+                while (pageNumber > 0)
+                {
+                    WebRequest request = WebRequest.Create(@probePage);
+                    WebResponse response = request.GetResponse();
+                    Stream data = response.GetResponseStream();
+                    string html = String.Empty;
+                    using (StreamReader sr = new StreamReader(data))
+                    {
+                        richTextBox2.AppendText(sr.ReadToEnd());
+                        richTextBox2.Refresh();
+                        // 
+                        pageNumber = Convert.ToInt32(label2.Text);
+                        label2.Text = Convert.ToString(pageNumber + 1);
+                        label2.Refresh();
+                        data.Close();
+                    }
+
+                    // ok done with this one close the webrequest make a new page entry and run again.
+
+                    if (richTextBox2.Find(@"Sorry.") > 0)
+                    {
+                        pageNumber = 0;
+                    }
+
+                    probePage = textBox1.Text + "?page=" + Convert.ToString(label2.Text);
+                    
+                }
+
+                // clean the data here.  This won't work for the web service
+
+                foreach (String lineRTB2 in richTextBox2.Lines)
+                {
+                    if (lineRTB2.Contains("span title") || lineRTB2.Contains("<td>"))
+                    {
+                        richTextBox1.AppendText(lineRTB2 + "\n");
+                        richTextBox1.Refresh();
+                    }
+                }
+
+                cleanRTB(); // ok clean it up and make ready for csv
+
+            }
+            catch
+            {
+                richTextBox1.AppendText("ERRERRERRERRERRERRERR");
+            }
+        }
+
+        private void cleanRTB() 
+        {
+            // ok, removes all the html tags and replaces them with a header line as the first line.  Then
+            // will concatinate in richTextBox2 in groups of 7 so that it maybe exported as a csv file.
+
+            // var for counting in groups of 7
+            int memberNumber = 1;
+            string catString = "";
+
+            richTextBox2.Clear();
+
+            // we will consume richTextBox1 line by line into richTextBox2 then clear richTextBox2 and move back over.
+            // yes there are other ways of doing this but I'm frigg'in lazy:)
+
+            richTextBox2.AppendText(@"Last|First|Title|Department|City|State" + "\n");  // send the header on over before bulk process
+
+            foreach (string lineInRTB1 in richTextBox1.Lines)
+            {
+                // concatinate string
+                catString += pullHTML(lineInRTB1.Trim());
+
+                memberNumber += 1;
+
+                // reset group'er
+                if (memberNumber == 8)
+                {
+                    richTextBox2.AppendText(catString);
+                    richTextBox2.AppendText(Environment.NewLine);
+
+                    catString = "";
+                    memberNumber = 1;
+                }
+                else
+                {
+                    catString += "|";
+                }
+            }
+
+            // move the finished product to richTextBox1
+            richTextBox1.Clear();
+
+            foreach (string lineInRTB2 in richTextBox2.Lines)
+            {
+                richTextBox1.AppendText(lineInRTB2);
+                richTextBox1.AppendText(Environment.NewLine);
+            }
+
+        }
+
+        private string pullHTML(string inString)
+        {
+            inString = inString.Replace("<span title=", "");
+            inString = inString.Replace("</span>", "");
+
+            inString = inString.Replace("<td>", "");
+            inString = inString.Replace("</td>", "");
+
+            inString = inString.Replace("&amp;", "");
+
+            inString = inString.Substring(inString.IndexOf(">") + 1);
+
+            // when it is done
+            return inString;
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
